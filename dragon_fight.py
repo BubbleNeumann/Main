694aa101dc5442e9df05a21e9952351988e00ad2
BubbleNeumann <dana@chkgo.com>
2019-08-19 18:21:07 +0400

    New variable and functionality: miss_probability. Were added: Hero's miss strikes and Hero's "set_shield" function. Fixed some outputs and controversial situations.

--- dragon_fight.py
+++ dragon_fight.py
@@ -1,6 +1,8 @@
-# TODO - add miss strikes
-# TODO - fix player interactive mode
+# TODO - fix player interactive mode: nice output statistic info
 # TODO - add alert "Wrong input!" and ability to reenter the command
+# TODO - welcome titles
+
+from random import randint
 
 
 def main():
@@ -9,19 +11,20 @@ def main():
     print(info)
 
     while dragon.health > 0 and hero.health > 0:
+        print("Dragon's health:", dragon.health,
+              "\nHero's health/stamina:", hero.health, '/', hero.stamina, "\n")
         hero.keyboard_inp(dragon)
-        dragon.attack(hero)
-        print("Dragon's health/stamina:", dragon.health, '/', dragon.stamina,
-              "\nHero's health/stamina:", hero.health, '/', hero.stamina)
+        if dragon.health > 0:
+            dragon.attack(hero)
         hero.stamina_refill()
         dragon.stamina_refill()
 
     if dragon.health <= 0:
-        print('Your hero won!')
+        print('\nYour Hero won!')
     else:
-        print('Your hero died.')
+        print('\nYour Hero died.')
 
-    wait = input()
+    wait = input("press enter to exit")
 
 
 class BattleUnit:
@@ -46,27 +49,30 @@ class BattleUnit:
 
 class Hero(BattleUnit):
 
-    default_health = 100
+    default_health = 10
     attack_force = 6
-    default_stammina = 15
+    default_stamina = 10
     shield = False
 
     def __init__(self):
         super().__init__(Hero.default_health, Hero.attack_force,
-                         Hero.default_stammina, Hero.shield)
+                         Hero.default_stamina, Hero.shield)
 
     def keyboard_inp(self, other):
         """Hero's move"""
         act = str(input())
-        if act == "A":
-            self.attack(other)
-        elif act == "S":
-            if not self.shield:
-                self.shield = True
-                self.stamina -= 3
+        if act == "A" or act == "a":
+            if self.stamina < 3:
+                miss_probability = randint(0, 3)
             else:
-                print('\nShield was already activated.')
-        elif act == "DS":
+                miss_probability = randint(0, 10)
+            if miss_probability == 0:
+                print("Your strike missed!")
+            else:
+                self.attack(other)
+        elif act == "S" or act == "s":
+            self.set_shield()
+        elif act == "DS" or act == "ds":
             if self.shield:
                 self.shield = False
             else:
@@ -76,14 +82,24 @@ class Hero(BattleUnit):
         if self.shield:
             self.stamina += 2
         else:
-            self.stamina += 10
-        if self.stamina > 15:
-            self.stamina = 15
+            self.stamina += 6
+        if self.stamina > 10:
+            self.stamina = 10
+
+    def set_shield(self):
+        if not self.shield and self.stamina >= 3:
+            self.shield = True
+            self.stamina -= 3
+            print("You've raised your shield.\n")
+        elif not self.shield and self.stamina < 3:
+            print('\nYour stamina level is too low to do it.')
+        else:
+            print('\nShield was already activated.')
 
 
 class Dragon(BattleUnit):
 
-    default_health = 12
+    default_health = 15
     attack_force = 5
     default_stamina = 10
     shield = False
@@ -95,6 +111,13 @@ class Dragon(BattleUnit):
     def stamina_refill(self):
         if self.stamina < Dragon.default_stamina:
             self.stamina = Dragon.default_stamina
+
+    def what_to_do(self, other):
+        """ Dragon analyses Hero's move, his stamina and health.
+            Then decides should he attack or not."""
+
+    def fire_attack(self, other):
+        """Special Dragon's move which causes more damage then usual attack."""
 
 
 info = ['[A] - to attack', '[S] - to rise a shield',
