bb099495fc80688f36e12fab5f13b484a77d6087
BubbleNeumann <dana@chkgo.com>
2019-08-21 17:37:19 +0400

    Welcome titles, class 'Level'. Basic location list, graphic display of the enemy's health.

--- dragon_fight.py
+++ dragon_fight.py
@@ -1,30 +1,43 @@
 # TODO - fix player interactive mode: nice output statistic info
 # TODO - add alert "Wrong input!" and ability to reenter the command
-# TODO - welcome titles
+# TODO - class "Location"
+# TODO - start shop location
 
 from random import randint
 
 
 def main():
+    print("All your progress will be lost if you stop the program.\n")
+    welcome()
     hero = Hero()
     dragon = Dragon()
     print(info)
 
-    while dragon.health > 0 and hero.health > 0:
-        print("Dragon's health:", dragon.health,
-              "\nHero's health/stamina:", hero.health, '/', hero.stamina, "\n")
-        hero.keyboard_inp(dragon)
-        if dragon.health > 0:
-            dragon.attack(hero)
-        hero.stamina_refill()
-        dragon.stamina_refill()
+    level = Level(dragon)
+    level.fight(hero, dragon)
 
-    if dragon.health <= 0:
-        print('\nYour Hero won!')
-    else:
-        print('\nYour Hero died.')
+    waiting()
 
-    wait = input("press enter to exit")
+
+def sep():
+    print('-----------------------------------------')
+
+
+def waiting():
+    wait = input("press enter to continue\n")
+
+
+def welcome():
+    title = str(input("Show introduction titles? [Y/N] - "))
+    sep()
+    if title == 'Y' or title == 'y':
+        print("\nWelcome to Great Kingdom, brave knight!\n"
+              "It's lend full of dangers, magic creatures and treasures.\n"
+              "The Dark Master appropriated our north lands.\n"
+              "King Arthur called you and asked you to kill him.\n"
+              "Your goal is to get to north lands and destroy Dark Magic at Great Kingdom.\n"
+              "Let's hit the road!\n")
+        waiting()
 
 
 class BattleUnit:
@@ -54,9 +67,11 @@ class Hero(BattleUnit):
     default_stamina = 10
     shield = False
 
-    def __init__(self):
+    def __init__(self, armor=False, wallet=100,):
         super().__init__(Hero.default_health, Hero.attack_force,
                          Hero.default_stamina, Hero.shield)
+        self.armor = armor
+        self.wallet = wallet
 
     def keyboard_inp(self, other):
         """Hero's move"""
@@ -108,18 +123,67 @@ class Dragon(BattleUnit):
         super().__init__(Dragon.default_health, Dragon.attack_force,
                          Dragon.default_stamina, Dragon.shield)
 
+    def intro(self):
+        """dragon description"""
+        print("")
+
     def stamina_refill(self):
         if self.stamina < Dragon.default_stamina:
             self.stamina = Dragon.default_stamina
-
-    def what_to_do(self, other):
-        """ Dragon analyses Hero's move, his stamina and health.
-            Then decides should he attack or not."""
 
     def fire_attack(self, other):
         """Special Dragon's move which causes more damage then usual attack."""
 
 
+class Level:
+
+    def __init__(self, enemy):
+        self.enemy = enemy
+
+    @staticmethod
+    def echo_health(self, enemy):
+        out = ''
+        for i in range(enemy.health):
+            out += '='
+        while len(out) < enemy.default_health:
+            out += '-'
+        return out
+
+    def what_to_do(self, other):
+        """ The enemy analyses Hero's move, his stamina and health.
+            Then decides should he attack or not.
+            Possible action: fire attack, usual attack, rise a shield"""
+
+    def fight(self, hero, enemy):
+        sep()
+        print('Get ready!\nFight starts!\n')
+        while enemy.health > 0 and hero.health > 0:
+            print("Dragon's health:", self.echo_health(self, enemy), "(", enemy.health, ')',
+                  "\nHero's health/stamina:", hero.health, '/', hero.stamina, "\n")
+            hero.keyboard_inp(enemy)
+            if enemy.health > 0:
+                enemy.attack(hero)
+            hero.stamina_refill()
+            enemy.stamina_refill()
+
+        if enemy.health <= 0:
+            print('\nYour Hero won!')
+        else:
+            print('\nYour Hero died.')
+        sep()
+
+
+class Location:
+    """List of locations:
+        1. Store (start point)
+        2. Field
+        3. The oak forest
+        N. Final location (final boss)"""
+
+
 info = ['[A] - to attack', '[S] - to rise a shield',
         '[DS] - to deactivate a shield']
+
+location_list = ['store', 'field', 'forest']
+
 main()
