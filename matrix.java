d74328b7627cae8547c14d531e41ba266a171ce0
BubbleNeumann <53640776+BubbleNeumann@users.noreply.github.com>
2021-07-04 10:04:20 +0400

    input mismatch exception handling

--- matrix.java
+++ matrix.java
@@ -1,43 +1,71 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
-// for graphical interfaces
-// import javax.swing.JOptionPane;
+class Matrix {
 
-class Main 
-{
-  public static void main(String[] args) 
-  {
-    
-    // String name = JOptionPane.showInputDialog("Enter your name");
-    // JOptionPane.showMessageDialog(null, "hello "+name);
-    
-    // int age = Integer.parceInt(JOptionPane.showinputDialog("Enter your age"))
-    // JOptionPane.showMessageDialog(null, "yyou are "+age+"years old");
-    
-    Scanner scanner = new Scanner(System.in);
-    
-    System.out.println("Number of the rows? ");
-    int row = scanner.nextInt();
-    
-    System.out.println("Number of the columns? ");
-    int col = scanner.nextInt();
-    
-    System.out.println(row);
-    System.out.println(col);
-    
-    int[] mx = new int[row*col];
-    
-    // String[][] cars = new String[3][3];
-    // cars[0][0] = "Mustang";
-    
-    // boolean result = name.equalsIgnoreCase("smth");
-    // int result = name.length();
-    
-    for(int i = row*col; i--;)
-    {
-      // fill an array
+    private int row, col;
+    private int body[];
+
+    // default constructior
+    public Matrix() {
+        
+        Scanner scanner = new Scanner(System.in);
+        boolean excCaught;
+
+        do {
+
+            excCaught = false;
+
+            try {
+              
+                System.out.println("Number of rows?");
+                row = scanner.nextInt();
+                
+                System.out.println("Number of columns?");
+                col = scanner.nextInt();
+            } catch(InputMismatchException e) {
+              
+                excCaught = true;
+                System.out.println("try again");
+                scanner.next();
+            }
+
+        } while (excCaught);
+        scanner.close();
+    }
+  
+    // the determinant is a scalar value that is a function of the entries of a square matrix
+    public int calcDeterminant() {
+        
+        int det;
+        
+        if (row != col) det = 0;
+        
+        return det;
     }
     
-    scanner.close();
-  }
-} 
+    // rank of a matrix A is the dimension of the vector space generated (or spanned) by its columns 
+    public int calcRank() {
+        
+        int rank(0);
+        return rank;
+    }
+
+    public void printMatrix() {
+        
+        for (int i = row; i > 0 ; i--) {
+            for (int j = col; j > 0; j--) {
+                System.out.print(body[(row-i)*col + col-j]);
+            }
+        }
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Matrix mx = new Matrix();
+
+    }
+}
