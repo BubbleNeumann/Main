c88e7f475207176e0dbed4b2175ee102e0227901
BubbleNeumann <53640776+BubbleNeumann@users.noreply.github.com>
2021-08-03 17:44:43 +0400

    some comments added

--- pythonTranslate.py
+++ pythonTranslate.py
@@ -1,8 +1,14 @@
+# pip install googletrans
+
 from googletrans import Translator
+
+# supports cyrillics, needed for Russian lang
 import codecs
 
 translator = Translator()
 
+
+# two files needed: (1) text.txt - source file, (2) translatoin.txt - the result
 inputFile = open("text.txt", 'r')
 outputFile = codecs.open("translation.txt", 'w', 'utf-8')
 
@@ -12,6 +18,7 @@ for line in inputFile:
 
     for i in range(len(sentences)-1):
 
+        # supports translations from English to Russian (use 'de' for German)
         translation = translator.translate(sentences[i], src = 'en', dest ='ru')
 
         outputFile.write(sentences[i] + " (" + translation.text + ").\n")
