c01f48038c8f6328dff77346e5a304bf57f70d24
BubbleNeumann <dana@chkgo.com>
2021-08-31 17:29:39 +0400

    ready to use

--- randomWebParser.py
+++ randomWebParser.py
@@ -1,20 +1,56 @@
 # required libraries:
-# beautufulsoup4, requests, lxml
+# beautufulsoup4, requests, lxml, googletrans
 
+# DONE add Google Translate
+# TODO write all in the text file (inside Dropbox folder?)
+# DONE remove "Username" in the beginning of each article
 
+from typing import List
+from googletrans import Translator
 from bs4 import BeautifulSoup
 import requests
 
-# GIGA GAMES
 
-source = requests.get('https://www.giga.de/games/').text
+def main():
+    source = requests.get('https://www.giga.de/games/').text
+    soup = BeautifulSoup(source, 'lxml')
 
-soup = BeautifulSoup(source, 'lxml')
+    # display the article headline
+    print(soup.find('a', class_='alice-teaser-link').getText(), '\n')
+    
+    link = soup.find('a', class_='alice-teaser-link').get('href')
 
-print(soup.prettify())
+    # follow the link to the first article on the main page
+    articlePage = requests.get('https:' + link).text
+    articleSoup = BeautifulSoup(articlePage, 'lxml')
+    articleText = ""
 
-# list of the articles' headers
+    for paragraph in articleSoup.find_all('p'):
+        articleText += paragraph.getText()
 
-for article in soup.find_all('a', class_ = 'alice-teaser-link'):
-  headline = article.text
-  print(headline)
+
+    articleText = articleText.removeprefix('Username')
+
+    # create a list of paragraphs, then slpit on sentences
+    paragraphList = articleText.split('\n')
+
+    for i in range(len(paragraphList)-1, 2, -1):
+        del paragraphList[i]
+
+    for i in range(len(paragraphList)-1):
+        sentenceList = paragraphList[i].split('. ')
+        translate(textOnGerman=sentenceList)
+
+
+def translate(textOnGerman):
+    translator = Translator()
+
+    for i in range(len(textOnGerman)-1):
+        translation = translator.translate(textOnGerman[i], src = 'de', dest ='en')
+
+        print(textOnGerman[i], '\n(', translation.text, ')')
+        print()
+
+
+if __name__ == '__main__':
+    main()
